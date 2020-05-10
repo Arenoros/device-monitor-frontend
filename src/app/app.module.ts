7f26bc92b6f232e5db931e86eb328ad50ce3d64d
Arenoros <arenoros@gmail.com>
2020-05-11 02:31:25 +0300

    add dialog

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,5 @@
 import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { NgModule, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 import { HttpClientModule } from '@angular/common/http';
 import { ReactiveFormsModule } from '@angular/forms';
 import { RouterModule, Routes } from '@angular/router';
@@ -14,17 +14,25 @@ import { MatIconModule } from '@angular/material/icon';
 import { MatToolbarModule} from '@angular/material/toolbar';
 import { MatTableModule } from '@angular/material/table';
 import { MatButtonModule } from '@angular/material/button';
-
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatDialogModule } from '@angular/material/dialog';
+import { MatInputModule } from '@angular/material/input';
+import { FormsModule } from '@angular/forms';
+import { MatSelectModule } from '@angular/material/select';
 import { AppComponent } from './app.component';
 import { TopBarComponent } from './top-bar/top-bar.component';
 
 import { DevicesViewComponent } from './devices-view/devices-view.component';
+import { AddDeviceComponent } from './add-device/add-device.component';
+import { MatRadioModule } from '@angular/material/radio';
+import { MatCardModule } from '@angular/material/card';
 
 @NgModule({
   declarations: [
     AppComponent,
     TopBarComponent,
-    DevicesViewComponent
+    DevicesViewComponent,
+    AddDeviceComponent
   ],
   imports: [
     BrowserModule,
@@ -43,9 +51,19 @@ import { DevicesViewComponent } from './devices-view/devices-view.component';
     MatIconModule,
     MatToolbarModule,
     MatTableModule,
-    MatButtonModule
+    MatButtonModule,
+    MatFormFieldModule,
+    MatDialogModule,
+    MatInputModule,
+    FormsModule,
+    MatSelectModule,
+    MatRadioModule,
+    MatCardModule
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
+  schemas: [
+    CUSTOM_ELEMENTS_SCHEMA,
+  ]
 })
 export class AppModule { }
--- src/app/devices-view/devices-view.component.ts
+++ src/app/devices-view/devices-view.component.ts
@@ -1,7 +1,10 @@
 import { Component, OnInit } from '@angular/core';
 import { DevicesService, DeviceInfo } from '../devices.service';
-import {MatIconRegistry} from '@angular/material/icon';
-import {DomSanitizer} from '@angular/platform-browser';
+import { MatIconRegistry } from '@angular/material/icon';
+import { DomSanitizer } from '@angular/platform-browser';
+import { MatDialogRef, MatDialog } from '@angular/material/dialog';
+import {AddDeviceComponent } from '../add-device/add-device.component';
+import { from } from 'rxjs';
 
 @Component({
   selector: 'app-devices-view',
@@ -15,6 +18,7 @@ export class DevicesViewComponent implements OnInit {
     private iconRegistry: MatIconRegistry,
     private sanitizer: DomSanitizer,
     private service: DevicesService,
+    public dialog: MatDialog,
   ) {
     iconRegistry.addSvgIcon('connected',
         sanitizer.bypassSecurityTrustResourceUrl('assets/connect_on.svg')
@@ -32,8 +36,16 @@ export class DevicesViewComponent implements OnInit {
   animal: string;
   name: string;
 
-  onAdd(): void{
+  onAdd(): void {
+    const dialogRef = this.dialog.open(AddDeviceComponent, {
+      id: 'add-device-id',
+      data: {name: this.name, animal: this.animal}
+    });
 
+    dialogRef.afterClosed().subscribe(result => {
+      console.log('The dialog was closed');
+      this.animal = result;
+    });
   }
   onUpdate(){
     this.service.imporFromGTable().subscribe(status =>
